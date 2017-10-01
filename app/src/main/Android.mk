3e973fa91f2953731a5a92afa04e47379dbccaa9
Steve Soltys <stevesoltys@live.com>
2017-10-01 16:39:13 -0400

    Rename backup transport whitelist configuration

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := com.stevesoltys.backup.xml
+LOCAL_MODULE := whitelist_com.stevesoltys.backup.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
