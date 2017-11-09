072e9a1b02694f97d975ccc8b40cd9d7a00647de
Steve Soltys <stevesoltys@live.com>
2017-11-08 23:53:45 -0500

    Remove Gradle as build system This application must be compiled with the operating system, so Gradle is unnecessary.

--- Android.mk
+++ Android.mk
@@ -21,6 +21,6 @@ LOCAL_PACKAGE_NAME := Backup
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := com.stevesoltys.backup.xml
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_SRC_FILES := $(call all-java-files-under, java)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 include $(BUILD_PACKAGE)
