c95655aa3a70fa427295f3b3ada236027bd29b9d
Steve Soltys <steve@stevesoltys.com>
2018-01-24 20:43:24 -0500

    Fix module dependencies in Makefile

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := Backup
 LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := com.stevesoltys.backup.xml
+LOCAL_REQUIRED_MODULES := permissions_com.stevesoltys.backup.xml whitelist_com.stevesoltys.backup.xml
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
