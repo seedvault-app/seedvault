523c4a32a49f4151f0a05d38d8ff4db899298e7e
Steve Soltys <steve@stevesoltys.com>
2018-11-10 16:01:31 -0500

    Set LOCAL_PRIVATE_PLATFORM_APIS flag for Android Pie support

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_PACKAGE_NAME := Backup
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := permissions_com.stevesoltys.backup.xml whitelist_com.stevesoltys.backup.xml
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 include $(BUILD_PACKAGE)
