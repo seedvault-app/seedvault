b17a55ac57f09f76c9a1b2735c1ecfe96f4eb249
Torsten Grote <t@grobox.de>
2019-06-11 10:25:36 -0300

    Target API 28

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -17,18 +17,19 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    commons-io:../../libs/commons-io-2.6.jar
+include $(BUILD_MULTI_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := Backup
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := permissions_com.stevesoltys.backup.xml whitelist_com.stevesoltys.backup.xml
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_CERTIFICATE := platform
 LOCAL_STATIC_JAVA_LIBRARIES := commons-io
 LOCAL_SRC_FILES := $(call all-java-files-under, java)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 include $(BUILD_PACKAGE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    commons-io:../../libs/commons-io-2.6.jar
-include $(BUILD_MULTI_PREBUILT)
