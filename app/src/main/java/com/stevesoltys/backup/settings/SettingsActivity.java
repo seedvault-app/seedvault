3d5911d41d50f2359b7e31f00ac8cd7a9485542a
Torsten Grote <t@grobox.de>
2019-07-03 19:44:37 +0200

    Add a SettingsViewModel in Kotlin including the Kotlin deps

--- app/src/main/java/com/stevesoltys/backup/settings/SettingsActivity.java
+++ app/src/main/java/com/stevesoltys/backup/settings/SettingsActivity.java
@@ -1,6 +1,10 @@
 package com.stevesoltys.backup.settings;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -8,13 +12,24 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.stevesoltys.backup.R;
 
+import static android.content.Intent.ACTION_OPEN_DOCUMENT_TREE;
+import static android.content.Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+import static android.widget.Toast.LENGTH_LONG;
 import static android.widget.Toast.LENGTH_SHORT;
+import static com.stevesoltys.backup.activity.MainActivity.OPEN_DOCUMENT_TREE_REQUEST_CODE;
 import static java.util.Objects.requireNonNull;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private final static String TAG = SettingsActivity.class.getName();
+
+    private SettingsViewModel viewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,7 +37,17 @@ public class SettingsActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_settings);
 
+        viewModel = ViewModelProviders.of(this).get(SettingsViewModel.class);
+
         requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!viewModel.locationIsSet()) {
+            showChooseFolderActivity();
+        }
     }
 
     @Override
@@ -49,4 +74,30 @@ public class SettingsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (resultCode != Activity.RESULT_OK) {
+            Log.w(TAG, "Error in activity result: " + requestCode);
+            return;
+        }
+
+        if (requestCode == OPEN_DOCUMENT_TREE_REQUEST_CODE) {
+            viewModel.handleChooseFolderResult(result);
+        }
+    }
+
+    private void showChooseFolderActivity() {
+        Intent openTreeIntent = new Intent(ACTION_OPEN_DOCUMENT_TREE);
+        openTreeIntent.addFlags(FLAG_GRANT_PERSISTABLE_URI_PERMISSION |
+                FLAG_GRANT_READ_URI_PERMISSION | FLAG_GRANT_WRITE_URI_PERMISSION);
+        try {
+            Intent documentChooser = Intent.createChooser(openTreeIntent, "Select the backup location");
+            startActivityForResult(documentChooser, OPEN_DOCUMENT_TREE_REQUEST_CODE);
+        } catch (ActivityNotFoundException ex) {
+            Toast.makeText(this, "Please install a file manager.", LENGTH_LONG).show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/stevesoltys/backup/settings/SettingsViewModel.kt
@@ -0,0 +1,28 @@
+package com.stevesoltys.backup.settings
+
+import android.app.Application
+import android.content.Intent
+import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
+import android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+import androidx.lifecycle.AndroidViewModel
+import com.stevesoltys.backup.settings.SettingsManager.getBackupFolderUri
+import com.stevesoltys.backup.settings.SettingsManager.setBackupFolderUri
+
+class SettingsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val app = application
+
+    fun locationIsSet() = getBackupFolderUri(getApplication()) != null
+
+    fun handleChooseFolderResult(result: Intent?) {
+        val folderUri = result?.data ?: return
+
+        // persist permission to access backup folder across reboots
+        val takeFlags = result.flags and (FLAG_GRANT_READ_URI_PERMISSION or FLAG_GRANT_WRITE_URI_PERMISSION)
+        app.contentResolver.takePersistableUriPermission(folderUri, takeFlags)
+
+        // store backup folder location in settings
+        setBackupFolderUri(app, folderUri)
+    }
+
+}
