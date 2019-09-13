4efaa0888153f87057f8bd7bb0c8f49b9075a27a
Torsten Grote <t@grobox.de>
2019-09-13 12:48:51 -0300

    Show notification right away when requesting manual backup

--- app/src/main/java/com/stevesoltys/backup/settings/SettingsViewModel.kt
+++ app/src/main/java/com/stevesoltys/backup/settings/SettingsViewModel.kt
@@ -1,6 +1,8 @@
 package com.stevesoltys.backup.settings
 
 import android.app.Application
+import com.stevesoltys.backup.Backup
+import com.stevesoltys.backup.R
 import com.stevesoltys.backup.transport.requestBackup
 import com.stevesoltys.backup.ui.RequireProvisioningViewModel
 
@@ -10,6 +12,10 @@ class SettingsViewModel(app: Application) : RequireProvisioningViewModel(app) {
 
     override val isRestoreOperation = false
 
-    fun backupNow() = Thread { requestBackup(app) }.start()
+    fun backupNow() {
+        val nm = (app as Backup).notificationManager
+        nm.onBackupUpdate(app.getString(R.string.notification_backup_starting), 0, 1, true)
+        Thread { requestBackup(app) }.start()
+    }
 
 }
