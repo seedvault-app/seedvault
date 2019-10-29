8686ee6c903b0d22bbd616733dd3dcdb62a9c2a8
Steve Soltys <steve@stevesoltys.com>
2019-10-28 23:53:47 -0400

    Fix APK deployment to prebuilt repo

--- deploy-prebuilt.sh
+++ deploy-prebuilt.sh
@@ -9,11 +9,11 @@ git config --global user.email "github@stevesoltys.com"
 git config --global user.name "Steve Soltys"
 git clone --quiet $REPO_URL
 
-cd backup-prebuilt
+cd seedvault-prebuilt
 git checkout $TRAVIS_BRANCH || git checkout -b $TRAVIS_BRANCH
 rm -Rf ./*
 cp $TRAVIS_BUILD_DIR/Android.mk .
-cp $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/app-release-unsigned.apk ./Backup.apk
+cp $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/app-release-unsigned.apk ./Seedvault.apk
 cp $TRAVIS_BUILD_DIR/permissions_com.stevesoltys.seedvault.xml .
 cp $TRAVIS_BUILD_DIR/whitelist_com.stevesoltys.seedvault.xml .
 
