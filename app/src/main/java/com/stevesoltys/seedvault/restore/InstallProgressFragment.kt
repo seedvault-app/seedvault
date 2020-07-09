0ce613b64dee0cd2a17c9b7abfe3542ac1c376aa
Michael Bestas <mkbestas@gmail.com>
2020-07-10 01:21:23 +0300

    Drop dependency on kotlin-android-extensions
    * Not available on AOSP.

--- app/src/main/java/com/stevesoltys/seedvault/restore/InstallProgressFragment.kt
+++ app/src/main/java/com/stevesoltys/seedvault/restore/InstallProgressFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
+import androidx.recyclerview.widget.RecyclerView
 import com.stevesoltys.seedvault.R
 import com.stevesoltys.seedvault.transport.restore.ApkRestoreStatus.QUEUED
 import com.stevesoltys.seedvault.transport.restore.InstallResult
 import com.stevesoltys.seedvault.transport.restore.getInProgress
-import kotlinx.android.synthetic.main.fragment_restore_progress.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class InstallProgressFragment : Fragment() {
@@ -23,9 +26,23 @@ class InstallProgressFragment : Fragment() {
     private val layoutManager = LinearLayoutManager(context)
     private val adapter = InstallProgressAdapter()
 
+    private lateinit var progressBar: ProgressBar
+    private lateinit var titleView: TextView
+    private lateinit var backupNameView: TextView
+    private lateinit var appList: RecyclerView
+    private lateinit var button: Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_restore_progress, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_restore_progress, container, false)
+
+        progressBar = v.findViewById(R.id.progressBar)
+        titleView = v.findViewById(R.id.titleView)
+        backupNameView = v.findViewById(R.id.backupNameView)
+        appList = v.findViewById(R.id.appList)
+        button = v.findViewById(R.id.button)
+
+        return v
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
