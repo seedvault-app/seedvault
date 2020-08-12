30e66f368ea266a9be60a89cdec51aa27619c34c
Torsten Grote <t@grobox.de>
2020-08-12 15:21:22 -0300

    Make PluginTest work for Nextcloud as well
    Only issue left was a different maximum file name length for Nextcloud

--- app/src/main/java/com/stevesoltys/seedvault/plugins/saf/DocumentsProviderKVBackup.kt
+++ app/src/main/java/com/stevesoltys/seedvault/plugins/saf/DocumentsProviderKVBackup.kt
@@ -2,11 +2,15 @@ package com.stevesoltys.seedvault.plugins.saf
 
 import android.content.Context
 import android.content.pm.PackageInfo
+import android.util.Log
 import androidx.documentfile.provider.DocumentFile
 import com.stevesoltys.seedvault.transport.backup.DEFAULT_QUOTA_KEY_VALUE_BACKUP
 import com.stevesoltys.seedvault.transport.backup.KVBackupPlugin
 import java.io.IOException
 import java.io.OutputStream
+
+const val MAX_KEY_LENGTH = 255
+const val MAX_KEY_LENGTH_NEXTCLOUD = 228
 
 @Suppress("BlockingMethodInNonBlockingContext")
 internal class DocumentsProviderKVBackup(
@@ -37,7 +41,8 @@ internal class DocumentsProviderKVBackup(
     override suspend fun removeDataOfPackage(packageInfo: PackageInfo) {
         // we cannot use the cached this.packageFile here,
         // because this can be called before [ensureRecordStorageForPackage]
-        val packageFile = storage.currentKvBackupDir?.findFileBlocking(context, packageInfo.packageName) ?: return
+        val packageFile =
+            storage.currentKvBackupDir?.findFileBlocking(context, packageInfo.packageName) ?: return
         packageFile.delete()
     }
 
@@ -54,6 +59,15 @@ internal class DocumentsProviderKVBackup(
         packageInfo: PackageInfo,
         key: String
     ): OutputStream {
+        check(key.length < MAX_KEY_LENGTH) {
+            "Key $key for ${packageInfo.packageName} is too long: ${key.length} chars."
+        }
+        if (key.length > MAX_KEY_LENGTH_NEXTCLOUD) {
+            Log.e(
+                DocumentsProviderKVBackup::class.simpleName,
+                "Key $key for ${packageInfo.packageName} is too long: ${key.length} chars."
+            )
+        }
         val packageFile = this.packageFile ?: throw AssertionError()
         packageFile.assertRightFile(packageInfo)
         val keyFile = packageFile.createOrGetFile(context, key)
