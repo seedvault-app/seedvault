447f1e5fb4409dfca75e53e53d71904980f5e0a2
Torsten Grote <t@grobox.de>
2020-10-23 10:25:44 -0300

    Move to different versioning scheme and show version in About dialog
    Change-Id: I87004c05f9a54d3b2f7854695349e8b506fa7e44

--- app/src/main/java/com/stevesoltys/seedvault/settings/AboutDialogFragment.kt
+++ app/src/main/java/com/stevesoltys/seedvault/settings/AboutDialogFragment.kt
@@ -8,9 +8,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.stevesoltys.seedvault.R
+import com.stevesoltys.seedvault.transport.backup.PackageService
+import org.koin.android.ext.android.inject
 
 class AboutDialogFragment : DialogFragment() {
 
+    private val packageService: PackageService by inject()
+
+    private lateinit var versionView: TextView
     private lateinit var licenseView: TextView
     private lateinit var authorView: TextView
     private lateinit var designView: TextView
@@ -24,10 +29,14 @@ class AboutDialogFragment : DialogFragment() {
                               savedInstanceState: Bundle?): View? {
         val v: View = inflater.inflate(R.layout.fragment_about, container, false)
 
+        versionView = v.findViewById(R.id.versionView)
         licenseView = v.findViewById(R.id.licenseView)
         authorView = v.findViewById(R.id.authorView)
         designView = v.findViewById(R.id.designView)
         sponsorView = v.findViewById(R.id.sponsorView)
+
+        val versionName = packageService.getVersionName(requireContext().packageName) ?: "???"
+        versionView.text = getString(R.string.about_version, versionName)
 
         return v
     }
