3082a974a2c9f19512eb2ff31d7987e9133b3eb2
Torsten Grote <t@grobox.de>
2021-02-22 11:26:21 -0300

    Add UI for restoring files after app restore

--- app/src/main/java/com/stevesoltys/seedvault/storage/Services.kt
+++ app/src/main/java/com/stevesoltys/seedvault/storage/Services.kt
@@ -1,10 +1,13 @@
 package com.stevesoltys.seedvault.storage
 
 import org.calyxos.backup.storage.api.BackupObserver
+import org.calyxos.backup.storage.api.RestoreObserver
 import org.calyxos.backup.storage.api.StorageBackup
 import org.calyxos.backup.storage.backup.BackupJobService
 import org.calyxos.backup.storage.backup.BackupService
 import org.calyxos.backup.storage.backup.NotificationBackupObserver
+import org.calyxos.backup.storage.restore.NotificationRestoreObserver
+import org.calyxos.backup.storage.restore.RestoreService
 import org.koin.android.ext.android.inject
 
 /*
@@ -26,5 +29,14 @@ internal class StorageBackupService : BackupService() {
     // use lazy delegate because context isn't available during construction time
     override val backupObserver: BackupObserver by lazy {
         NotificationBackupObserver(applicationContext)
+    }
+}
+
+internal class StorageRestoreService : RestoreService() {
+    override val storageBackup: StorageBackup by inject()
+
+    // use lazy delegate because context isn't available during construction time
+    override val restoreObserver: RestoreObserver by lazy {
+        NotificationRestoreObserver(applicationContext)
     }
 }
