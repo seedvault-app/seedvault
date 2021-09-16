e4553c191a8f41ba3410f98fd9301faa186264ba
Torsten Grote <t@grobox.de>
2021-09-16 18:11:37 +0200

    Add crypto method to get salted names for package data and APKs
    This will only hide installed apps from naive attackers as the APKs are still not encrypted and even then other attacks would be possible.
    
    However, it allows us to simplify our storage plugin API.

--- app/src/test/java/com/stevesoltys/seedvault/crypto/CryptoImplTest.kt
+++ app/src/test/java/com/stevesoltys/seedvault/crypto/CryptoImplTest.kt
@@ -1,7 +1,12 @@
 package com.stevesoltys.seedvault.crypto
 
+import com.stevesoltys.seedvault.getRandomBase64
+import com.stevesoltys.seedvault.getRandomString
 import com.stevesoltys.seedvault.header.HeaderReaderImpl
+import com.stevesoltys.seedvault.metadata.METADATA_SALT_SIZE
 import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -24,6 +29,66 @@ class CryptoImplTest {
         assertThrows(IOException::class.java) {
             crypto.decryptMultipleSegments(inputStream)
         }
+    }
+
+    @Test
+    fun `getNameForPackage() and getNameForApk() return deterministic results`() {
+        val salt = getRandomBase64(METADATA_SALT_SIZE)
+        val packageName = getRandomString(32)
+        assertEquals(
+            crypto.getNameForPackage(salt, packageName),
+            crypto.getNameForPackage(salt, packageName)
+        )
+        assertEquals(
+            crypto.getNameForApk(salt, packageName),
+            crypto.getNameForApk(salt, packageName)
+        )
+    }
+
+    @Test
+    fun `getNameForPackage() and getNameForApk() return different results`() {
+        val salt = getRandomBase64(METADATA_SALT_SIZE)
+        val packageName = getRandomString(32)
+        assertNotEquals(
+            crypto.getNameForPackage(salt, packageName),
+            crypto.getNameForApk(salt, packageName)
+        )
+    }
+
+    @Test
+    fun `getNameForPackage() and getNameForApk() return different results for different salts`() {
+        val packageName = getRandomString(32)
+        assertNotEquals(
+            crypto.getNameForPackage(getRandomBase64(METADATA_SALT_SIZE), packageName),
+            crypto.getNameForPackage(getRandomBase64(METADATA_SALT_SIZE), packageName)
+        )
+        assertNotEquals(
+            crypto.getNameForApk(getRandomBase64(METADATA_SALT_SIZE), packageName),
+            crypto.getNameForApk(getRandomBase64(METADATA_SALT_SIZE), packageName)
+        )
+    }
+
+    @Test
+    fun `getNameForPackage() and getNameForApk() return different for different package names`() {
+        val salt = getRandomBase64(METADATA_SALT_SIZE)
+        assertNotEquals(
+            crypto.getNameForPackage(salt, getRandomString(32)),
+            crypto.getNameForPackage(salt, getRandomString(32))
+        )
+        assertNotEquals(
+            crypto.getNameForApk(salt, getRandomString(32)),
+            crypto.getNameForApk(salt, getRandomString(32))
+        )
+    }
+
+    @Test
+    fun `getNameForApk() return different results for different suffixes`() {
+        val salt = getRandomBase64(METADATA_SALT_SIZE)
+        val packageName = getRandomString(32)
+        assertNotEquals(
+            crypto.getNameForApk(salt, packageName, getRandomString(4)),
+            crypto.getNameForApk(salt, packageName, getRandomString(5))
+        )
     }
 
 }
