21f1667a1fd83336f0a5e10e705db296a20ae842
Chirayu Desai <chirayudesai1@gmail.com>
2022-01-01 04:43:15 +0530

    Import and convert SettingsLib.Utils.getColorAccent
    https: //cs.android.com/android/platform/superproject/+/android-12.0.0_r1:frameworks/base/packages/SettingsLib/src/com/android/settingslib/Utils.java?q=SettingsLib.Utils
    Change-Id: Ieab581569ebaa6f45529c09ad75c1037e93fa772

--- app/src/main/java/com/stevesoltys/seedvault/ui/UiUtils.kt
+++ app/src/main/java/com/stevesoltys/seedvault/ui/UiUtils.kt
@@ -1,6 +1,7 @@
 package com.stevesoltys.seedvault.ui
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.text.format.DateUtils.MINUTE_IN_MILLIS
 import android.text.format.DateUtils.getRelativeTimeSpanString
 import com.stevesoltys.seedvault.R
@@ -12,4 +13,21 @@ fun Long.toRelativeTime(context: Context): CharSequence {
         val now = System.currentTimeMillis()
         getRelativeTimeSpanString(this, now, MINUTE_IN_MILLIS, 0)
     }
+}
+
+// Ported from
+// https://cs.android.com/android/platform/superproject/+/android-12.0.0_r1:frameworks/base/packages/SettingsLib/src/com/android/settingslib/Utils.java
+fun getColorAccent(context: Context): ColorStateList? {
+    return getColorAttr(context, android.R.attr.colorAccent)
+}
+
+fun getColorAttr(context: Context, attr: Int): ColorStateList? {
+    val typedArray = context.obtainStyledAttributes(IntArray(1){ attr })
+    var stateList: ColorStateList? = null
+    try {
+        stateList = typedArray.getColorStateList(0)
+    } finally {
+        typedArray.recycle()
+    }
+    return stateList
 }
