fac6f2ea55fcb6006f14b389100275d76c8313fb
Michael Bestas <mikeioannina@gmail.com>
2023-03-02 18:15:28 +0200

    storage: Add SPDX copyright headers

--- storage/demo/src/main/java/de/grobox/storagebackuptester/settings/InfoFragment.kt
+++ storage/demo/src/main/java/de/grobox/storagebackuptester/settings/InfoFragment.kt
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package de.grobox.storagebackuptester.settings
 
 import android.os.Bundle
--- storage/lib/src/main/java/org/calyxos/backup/storage/plugin/saf/DocumentFileExt.kt
+++ storage/lib/src/main/java/org/calyxos/backup/storage/plugin/saf/DocumentFileExt.kt
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 @file:Suppress("BlockingMethodInNonBlockingContext")
 
 package org.calyxos.backup.storage.plugin.saf
