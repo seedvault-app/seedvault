b12e4559db372e477f826898998c9e7be21d5072
Michael Bestas <mikeioannina@gmail.com>
2023-03-02 18:41:32 +0200

    contactsbackup: Add SPDX copyright headers

--- contactsbackup/src/main/java/org/calyxos/backup/contacts/FullBackupFileHandler.java
+++ contactsbackup/src/main/java/org/calyxos/backup/contacts/FullBackupFileHandler.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2020 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.calyxos.backup.contacts;
 
 import android.app.backup.FullBackupDataOutput;
