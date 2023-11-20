18013c03796aa2fd0fe857279516d5382df171fa
Tommy Webb <tommy@calyxinstitute.org>
2023-11-20 10:03:44 -0500

    Support installing via F-Droid Basic
    If an app being restored was originally installed with F-Droid Basic,
    try to install it using F-Droid Basic again.
    
    Change-Id: Ib067389ba20d74596892e24efd561ab2918d62cb

--- app/src/main/java/com/stevesoltys/seedvault/restore/install/InstallIntentCreator.kt
+++ app/src/main/java/com/stevesoltys/seedvault/restore/install/InstallIntentCreator.kt
@@ -14,6 +14,7 @@ internal class InstallIntentCreator(
     private val installerToPackage = mapOf(
         "org.fdroid.fdroid" to "org.fdroid.fdroid",
         "org.fdroid.fdroid.privileged" to "org.fdroid.fdroid",
+        "org.fdroid.basic" to "org.fdroid.basic",
         "com.aurora.store" to "com.aurora.store",
         "com.aurora.services" to "com.aurora.store",
         "com.android.vending" to "com.android.vending"
