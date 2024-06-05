1061cdeed320d0e19eb1a5f75f0592fa6bc7fbcd
Steve Soltys <steve@stevesoltys.com>
2024-06-05 16:42:32 -0400

    Fix tests

--- app/src/androidTest/java/com/stevesoltys/seedvault/e2e/SeedvaultLargeTest.kt
+++ app/src/androidTest/java/com/stevesoltys/seedvault/e2e/SeedvaultLargeTest.kt
@@ -16,6 +16,7 @@ import org.junit.rules.TestName
 import org.junit.runner.RunWith
 import org.koin.core.component.KoinComponent
 import java.io.File
+import java.lang.Thread.sleep
 import java.util.concurrent.atomic.AtomicBoolean
 
 @RunWith(AndroidJUnit4::class)
@@ -43,6 +44,9 @@ internal abstract class SeedvaultLargeTest :
     open fun setUp() = runBlocking {
         resetApplicationState()
         clearTestBackups()
+
+        sleep(120000)
+        runCommand("bmgr transport com.stevesoltys.seedvault.transport.ConfigurableBackupTransport")
 
         startRecordingTest(keepRecordingScreen, name.methodName)
         restoreBaselineBackup()
