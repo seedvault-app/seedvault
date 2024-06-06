e4f8485d4ac108138de2f36aa94b581465d75f44
Steve Soltys <steve@stevesoltys.com>
2024-06-05 22:29:45 -0400

    Fix tests

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -2,11 +2,14 @@ echo "Settings transport to Seedvault..."
 index=0
 
 adb shell bmgr autorestore false
+adb install -r app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+sleep 60
 
 D2D_BACKUP_TEST=$1
 
 large_test_exit_code=0
-./gradlew --stacktrace -Pinstrumented_test_size=large -Pd2d_backup_test="$D2D_BACKUP_TEST" :app:connectedAndroidTest || large_test_exit_code=$?
+adb shell am instrument -w -e package com.stevesoltys.seedvault.e2e com.stevesoltys.seedvault.test/com.stevesoltys.seedvault.KoinInstrumentationTestRunner || large_test_exit_code=$?
+#./gradlew --stacktrace -Pinstrumented_test_size=large -Pd2d_backup_test="$D2D_BACKUP_TEST" :app:connectedAndroidTest || large_test_exit_code=$?
 
 adb pull /sdcard/seedvault_test_results
 
