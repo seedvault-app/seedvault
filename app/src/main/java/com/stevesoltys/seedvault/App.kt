9f4d0f6070e90a2d6b3cf2b614d03373b8b1b1a1
HeroBuxx <me@herobuxx.me>
2025-03-07 20:46:58 +0700

    Simplify debug build check logic
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- app/src/main/java/com/stevesoltys/seedvault/App.kt
+++ app/src/main/java/com/stevesoltys/seedvault/App.kt
@@ -193,7 +193,7 @@ const val ERROR_BACKUP_CANCELLED: Int = BackupManager.ERROR_BACKUP_CANCELLED
 const val ERROR_BACKUP_NOT_ALLOWED: Int = BackupManager.ERROR_BACKUP_NOT_ALLOWED
 
 // TODO this doesn't work for LineageOS as they do public debug builds
-fun isDebugBuild() = Build.TYPE == "userdebug" || Build.TYPE == "eng"
+fun isDebugBuild() = Build.TYPE != "user"
 
 fun <T> permitDiskReads(func: () -> T): T {
     return if (isDebugBuild()) {
