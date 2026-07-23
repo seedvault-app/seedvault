e15bc15e3051edd0fd315a2e405ec633b8a4e549
althafvly <althafvly@gmail.com>
2026-07-23 11:43:54 +0530

    Exclude SMS provider from backup on non-main users

--- app/src/main/java/com/stevesoltys/seedvault/ui/SystemData.kt
+++ app/src/main/java/com/stevesoltys/seedvault/ui/SystemData.kt
@@ -5,6 +5,7 @@
 
 package com.stevesoltys.seedvault.ui
 
+import android.os.UserHandle
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.stevesoltys.seedvault.R
@@ -15,12 +16,17 @@ internal const val PACKAGE_NAME_CALL_LOG = "com.android.calllogbackup"
 internal const val PACKAGE_NAME_CONTACTS = "org.calyxos.backup.contacts"
 internal const val PACKAGE_NAME_SYSTEM = "@org.calyxos.system@"
 
-val systemData = mapOf(
-    PACKAGE_NAME_SMS to SystemData(R.string.backup_sms, R.drawable.ic_message),
-    PACKAGE_NAME_SETTINGS to SystemData(R.string.backup_settings, R.drawable.ic_settings),
-    PACKAGE_NAME_CALL_LOG to SystemData(R.string.backup_call_log, R.drawable.ic_call),
-    PACKAGE_NAME_CONTACTS to SystemData(R.string.backup_contacts, R.drawable.ic_contacts),
-)
+val systemData = buildMap {
+    // SMS/MMS data is stored only for the primary user - android:singleUser="true".
+    // On secondary user, the framework skips SMS/MMS backup, so don't show it.
+    if (UserHandle.myUserId() == UserHandle.USER_SYSTEM) {
+        put(PACKAGE_NAME_SMS, SystemData(R.string.backup_sms, R.drawable.ic_message))
+    }
+
+    put(PACKAGE_NAME_SETTINGS, SystemData(R.string.backup_settings, R.drawable.ic_settings))
+    put(PACKAGE_NAME_CALL_LOG, SystemData(R.string.backup_call_log, R.drawable.ic_call))
+    put(PACKAGE_NAME_CONTACTS, SystemData(R.string.backup_contacts, R.drawable.ic_contacts))
+}
 
 data class SystemData(
     @StringRes val nameRes: Int,
